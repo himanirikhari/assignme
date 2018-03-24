31be0c47daa313b180d24ad92d7ae84046ed15b3
himani <rahubora11@gmail.com>
2018-03-24 19:56:43 +0000

    change in data json

--- src/data/data.js
+++ src/data/data.js
@@ -3,11 +3,11 @@
 
 const data=[
     
-    {Equipment:"A", Image:"", Name:"Machine A",Vendor: "vendor A",Location:"Delhi",Model:"",Serial:"", Description:""},
-    {Equipment:"B", Image:"", Name:"Machine B",Vendor: "Vendor B",Location:"Mumbai",Model:"",Serial:"", Description:""},
-    {Equipment:"C", Image:"", Name:"Machine C",Vendor: "Vendor C",Location:"Bangalore",Model:"",Serial:"", Description:""},
-    {Equipment:"D", Image:"", Name:"Machine D",Vendor: "Vendor D",Location:"Hyderabad",Model:"",Serial:"", Description:""}
-
+    {Equipment:"A", Image:"", Name:"Machine A",Vendor: "Vendor A",Location:"Delhi",Model:"Model A",Serial:"567432", Description:"This is Description of Equipment A"},
+    {Equipment:"B", Image:"", Name:"Machine B",Vendor: "Vendor B",Location:"Mumbai",Model:"Model B",Serial:"9876445", Description:"This is Description of Equipment B"},
+    {Equipment:"C", Image:"", Name:"Machine C",Vendor: "Vendor C",Location:"Bangalore",Model:"Model C",Serial:"657949", Description:"This is Description of Equipment C"},
+    {Equipment:"D", Image:"", Name:"Machine D",Vendor: "Vendor D",Location:"Bangalore",Model:"Model D",Serial:"657949", Description:"This is Description of Equipment D"},
+ 
 ];
 
 export default data;
